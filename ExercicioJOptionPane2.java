45ee54793d80d6d0c3c0b412718fd11b0047473d
Gabriel de Oliveira Ferreira <gabrieloferreira.u4@gmail.com>
2018-04-20 10:50:05 -0300

    20-04-18-10-50
    CEDUP

--- ExercicioJOptionPane2.java
+++ ExercicioJOptionPane2.java
@@ -4,10 +4,19 @@ public class ExercicioJOptionPane2{
 
     public static void main(String[] args) {
         
-        int litros = Integer.parseInt(JOptionPane.showInputDialog(null, "Litros de água por dia"));
-        int periodo = Integer.parseInt(JOptionPane.showInputDialog(null, "Periodo consumido(em anos)"));
-        int valor = Integer.parseInt(JOptionPane.showInputDialog(null, "Preço por litro"));
+        int litrosDia = Integer.parseInt(JOptionPane.showInputDialog(null, "Litros de água por dia"));
+        int periodoAnos = Integer.parseInt(JOptionPane.showInputDialog(null, "Periodo consumido(em anos)"));
+        double precoLitro = Double.parseDouble(JOptionPane.showInputDialog(null, "Preço por litro"));
 
-        
+        int periodoDias = periodoAnos*365;
+        int litrosTotal = litrosDia*periodoDias;
+        double valorTotal = litrosTotal*precoLitro;
+
+        JOptionPane.showMessageDialog(null, "Quantidade de litros por dia: " + litrosDia+
+        "\nQuantidade de anos consumidos: " + periodoAnos +
+        "\n Valor por Litro: " + precoLitro + "\n " +
+        "\nQuantidade de litros consumidos: " + litrosTotal +
+        "\nValor Total Pago: " + "R$ " + valorTotal
+        );
      }
 }
